c80f27ed85590d687c2e1731d2df94886643e997
alexcerezo <alexcerezocontreras@gmail.com>
2025-01-27 19:33:28 +0100

    First steps on creating the dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import React from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import ThemeContext from "./context/ThemeContext";
 
 function App() {
-  return <Dashboard />;
+  const [darkMode, setDarkMode] = React.useState(false);
+  return (
+    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+      <Dashboard />
+    </ThemeContext.Provider>
+  );
 }
 
 export default App;
