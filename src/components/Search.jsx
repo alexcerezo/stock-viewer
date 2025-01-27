11207d5a8d0faa190fc008f132533d898ed76280
alexcerezo <alexcerezocontreras@gmail.com>
2025-01-27 16:52:05 +0100

    Adding search icons and configuring tailwind

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
+import { mockSearchResults } from '../constants/mock';
+import { MagnifyingGlassIcon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Search = () => {
     const [input, setInput] = useState('');
-    const [bestMatches, setBestMatches] = useState([]);
+    const [bestMatches, setBestMatches] = useState(mockSearchResults.result);
 
     const updateBestMatches = () => {
         setBestMatches(mockSearchResults.result);
     }
 
+    const clear = () => {
+        setInput('');
+        setBestMatches([]);
+    }
+
     return (
-        <div className='flex item-center my-4 border-2 rounded-md relative z-50 w-96 bg-white border-neutral-200'>
+        <div className='flex items-center my-4 border-2 rounded-md relative z-50 w-96 bg-white border-neutral-200'>
             <input type="text"
                 value={input}
                 placeholder="Search stocks..."
@@ -21,6 +28,15 @@ const Search = () => {
                     }
                 }}
             />
+            {input && (
+                <button onClick={clear} className='p-3'>
+                    <XMarkIcon className="h-4 w-4 fill-gray-500" />
+                </button>
+            )}
+            
+            <button onClick={updateBestMatches} className='h-8 w-8 bg-indigo-600 rounded-md flex justify-center items-center m-1 p-2 text-white'>
+                <MagnifyingGlassIcon className='h-4 w-4 fill-gray-100'/>
+            </button>
         </div>
     );
 };
